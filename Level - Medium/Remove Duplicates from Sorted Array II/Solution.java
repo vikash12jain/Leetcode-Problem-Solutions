fe5ed0140138ce18874f8316c95f8c9e39067cb3
vikash12jain <vikash12jain@gmail.com>
2025-04-29 20:10:09 +0530

    update

--- Level - Medium/Remove Duplicates from Sorted Array II/Solution.java
+++ Level - Medium/Remove Duplicates from Sorted Array II/Solution.java
@@ -1,9 +1,26 @@
 class Solution {
-    static public int removeDuplicates(int[] nums) {
-         return 0;
-     }
+    static public int removeDuplicates(int[] nums)  {
+        int n = nums.length;
+        if (n <= 2) return n;
+    
+        int index = 2;
+    
+        for (int j = 2; j < n; j++) {
+            if (nums[j] != nums[index - 2]) {
+                nums[index] = nums[j];
+                index++;
+            }
+        }
+    
+        return index;
+    }
+    
+
      public static void main(String[] args) {
-         int [] arr = {3,1,4,4};
+         int [] arr = {1,1,1,2,2,3};
          System.out.println(removeDuplicates(arr));
+         for (int i : arr) {
+            System.out.print(i);
+         }
      }
  }
