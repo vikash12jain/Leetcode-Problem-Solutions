5ae0405a1499d3f2a7f5fba3afe5018b0691cc06
vikash12jain <vikash12jain@gmail.com>
2025-04-29 09:27:26 +0530

    update

--- Level - Easy/Remove duplicates from Sorted array/Solution.java
+++ Level - Easy/Remove duplicates from Sorted array/Solution.java
@@ -1,9 +1,21 @@
 class Solution {
-   static public int removeDuplicates(int[] nums) {
-        return 0;
+    static public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+   return i+1;
     }
+
     public static void main(String[] args) {
-        int [] arr = {3,1,4,4};
+        int[] arr = {0,0,3,3,5,5,6,8,9,9 };
         System.out.println(removeDuplicates(arr));
+        for (int i : arr) {
+            System.out.print(i);
+        }
     }
 }
