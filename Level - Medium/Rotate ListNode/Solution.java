f79a34b0c65402673763318d60274bd65ee541ab
vikash12jain <vikash12jain@gmail.com>
2025-05-01 11:48:25 +0530

    update

---
+++ Level - Medium/Rotate ListNode/Solution.java
@@ -0,0 +1,83 @@
+public class Solution {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static public void insertAtEnd(ListNode head, int value) {
+        ListNode newNode = new ListNode(value);
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    static public void traverse(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.print("Null");
+        }
+        System.out.println("");
+    }
+
+    static public ListNode rotateRight(ListNode head, int k) {
+        if (k <= 0 || head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        int length = 1;
+        ListNode prevcurrent = head;
+        while (current.next != null) {
+            length++;
+            prevcurrent = current;
+            current = current.next;
+        }
+
+        // Handle cases where k is greater than the length of the list
+        k = k % length;
+        if (k == 0) return head; // If no rotation is needed, return the head
+
+        // Break the list and perform the rotation
+        prevcurrent.next = null; // Disconnect the last node
+        ListNode newHead = current; // The last node will be the new head
+        current.next = head; // Point the last node to the original head
+        head = newHead; // Update the head to the new head
+
+        // Now, we rotate (k-1) times since we have already moved the last element
+        head = rotateRight(head, k - 1);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        insertAtEnd(head, 2);
+        insertAtEnd(head, 3);
+        insertAtEnd(head, 4);
+        insertAtEnd(head, 5);
+
+        System.out.println("Original list:");
+        traverse(head);
+
+        head = rotateRight(head, 1000); // Test with k > length
+        System.out.println("List after rotation:");
+        traverse(head);
+    }
+}
