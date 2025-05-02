560807d3f885b186c8740e68fd59f669c4ee363a
vikash12jain <vikash12jain@gmail.com>
2025-05-02 22:06:43 +0530

    update

--- Level - Medium/Longest Substring with At Least K Repeating Characters/Solution.java
+++ Level - Medium/Longest Substring with At Least K Repeating Characters/Solution.java
@@ -1,24 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-   static public int longestSubstring(String s, int k) {
-        int maxLength = 0;
-        Map<Character, Integer> freq = new HashMap<>();
+public class Solution{
+    static public int longestSubstring(String s, int k) {
+        if (s == null || s.length() == 0 || s.length() < k)
+            return 0;
 
-        for (int end = 0; end < s.length(); end++) {
-            freq.put(s.charAt(end), freq.getOrDefault(s.charAt(end), 0)+1);
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
-        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
-            if(entry.getValue()>=k){
-                maxLength +=entry.getValue();
+        for (char ch : freq.keySet()) {
+            if (freq.get(ch) < k) {
+                String[] parts = s.split(String.valueOf(ch));
+
+                int max = 0;
+                for (String part : parts) {
+                    max = Math.max(max, longestSubstring(part, k));
+                }
+                return max;
             }
         }
-        return maxLength;
+
+        return s.length();
     }
-public static void main(String[] args) {
-    String s = "aaabb";
-    int k = 3;
-    System.out.println(longestSubstring(s, k));
-}    
+
+    public static void main(String[] args) {
+        String s = "ababacb";
+        int k = 3;
+        System.out.println(longestSubstring(s, k));
+    }
 }
