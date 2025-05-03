1e380691ad554bcb4e5ed0732e2416174af77295
vikash12jain <vikash12jain@gmail.com>
2025-05-03 20:02:23 +0530

    update

--- Level - Medium/Minimum Domino Rotations For Equal Row/Solution.java
+++ Level - Medium/Minimum Domino Rotations For Equal Row/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
+   static public int minDominoRotations(int[] tops, int[] bottoms) {
         int rotations = Math.min(check(tops[0], tops, bottoms), check(bottoms[0], tops, bottoms));
         return rotations == Integer.MAX_VALUE ? -1 : rotations;
     }
 
-    private int check(int x, int[] tops, int[] bottoms) {
+  static private int check(int x, int[] tops, int[] bottoms) {
         int rotationsTop = 0;
         int rotationsBottom = 0;
 
         for (int i = 0; i < tops.length; i++) {
             if (tops[i] != x && bottoms[i] != x) {
-                return Integer.MAX_VALUE; // Not possible
+                return Integer.MAX_VALUE;
             } else if (tops[i] != x) {
                 rotationsTop++;
             } else if (bottoms[i] != x) {
@@ -19,5 +19,10 @@ class Solution {
         }
 
         return Math.min(rotationsTop, rotationsBottom);
+    }
+    public static void main(String[] args) {
+        int [] top = {5,2,6,2,3,2};
+        int [] bottom = {2,1,2,4,2,2};
+        System.out.println(minDominoRotations(top, bottom));
     }
 }
