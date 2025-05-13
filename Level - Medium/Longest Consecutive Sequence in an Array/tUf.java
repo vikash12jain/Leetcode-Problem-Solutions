24e7f14eb05dde039ffbc702bb8f5b4521f5f2b6
vikash12jain <vikash12jain@gmail.com>
2025-05-13 12:23:48 +0530

    add new files

--- Level - Medium/Longest Consecutive Sequence in an Array/tUf.java
+++ Level - Medium/Longest Consecutive Sequence in an Array/tUf.java
@@ -1,41 +1,39 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
-public class tUf{
-    public static int longestSuccessiveElements(int[] a) {
-        int n = a.length;
-        if (n == 0)
-            return 0;
-
-        int longest = 1;
-        Set<Integer> set = new HashSet<>();
-
-        // put all the array elements into set
-        for (int i = 0; i < n; i++) {
-            set.add(a[i]);
-        }
-
-        // Find the longest sequence
-        for (int it : set) {
-            // if 'it' is a starting number
-            if (!set.contains(it - 1)) {
-                // find consecutive numbers
-                int cnt = 1;
-                int x = it;
-                while (set.contains(x + 1)) {
-                    x = x + 1;
-                    cnt = cnt + 1;
-                }
-                longest = Math.max(longest, cnt);
+public class tUf {
+static public Boolean linearsearch(int[] a, int x) {
+        for (int i : a) {
+            if (i == x) {
+                return true;
             }
         }
-        return longest;
+        return false;
+    }
+
+    static public int longsequence(int[]a){
+        int n = a.length;
+        int length = 1, cnt = 1;
+        if(n ==0)return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i : a) {
+           set.add(i); 
+        }
+     for (Integer i : set) {
+        if(!set.contains(i-1)){
+            int x = i+1;
+            while(linearsearch(a, x)){
+                x++;cnt++;
+            }
+            length = Math.max(length, cnt);
+            cnt = 1;
+        }
+     }
+     return length;
     }
 
     public static void main(String[] args) {
-        int[] a = {100, 200, 1, 2, 3, 4};
-        int ans = longestSuccessiveElements(a);
-        System.out.println("The longest consecutive sequence is " + ans);
+        int [] srr = {9,9,9,5,1,3,2,4,8,7};
+        System.out.println(longsequence(srr));
     }
 }
-
-
